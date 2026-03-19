4d44eec08d7b7216c142c13bd9e22acd11a8e3aa
josamcode <gergessamuel100@gmail.com>
2026-03-19 07:43:36 +0200

    update allowed origins

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 // Allowed origins
 const allowedOrigins =
   process.env.NODE_ENV === 'production'
-    ? ['https://ararm.vercel.app', 'https://ararmsai']
+    ? ['https://ararm.vercel.app', 'https://ararmsai.com', 'https://www.ararmsai.com']
     : [
       'http://localhost:3000',
       'http://192.168.56.1:3000',
